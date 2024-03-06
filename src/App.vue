ce8c641225ff9489043012a9f4afa2eabb13454a
1111PP <1156912249@qq.com>
2024-03-06 21:44:36 +0800

    第三次提交测试代码

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 let func = () => {
-    }
-    console.log(func);
+    console.log(1);
+}
+console.log(func);
 </script>
 
 <template>
     <h1>提交没有格式化的代码</h1>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
