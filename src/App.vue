1088be744456056def8b2e2c2b5e66f4576ab040
1111PP <1156912249@qq.com>
2024-03-06 21:23:07 +0800

    提交一次错误代码

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+let func = () => {
+        // dasdasdas
+    }
+    console.log(func);
 </script>
 
 <template>
-    <h1>1</h1>
+    <h1>提交没有格式化的代码</h1>
 </template>
 
 <style scoped>
