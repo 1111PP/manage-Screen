e83717b8e6971daf054b8bff69fb538c7f2193e8
1111PP <1156912249@qq.com>
2024-03-31 21:55:22 +0800

    dist latest2

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { RouterLink, useRouter, RouterView, useRoute } from "vue-router";
+// console.log(1);
+function a() {
+    const a = 1
+    return a
+}
+const b = ref<number>(a())
 </script>
 
 <template>
+    <!-- 1 -->
     <RouterView> </RouterView>
 </template>
 
